a8f8d8820d9ec75c7b77ca14bc297ab644ee0d3e
FoyArthur <20FoyA06@stu.smuhsd.org>
2018-09-07 08:43:39 -0700

    commit

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,10 @@
+/* This class contains the methods to do various math related topics
+ * @author Arthur Foy
+ * @version 9/6/2018
+ */
+public class Calculate {
+
+	public static int square(int number) {
+		return number * number;
+	}
+}
