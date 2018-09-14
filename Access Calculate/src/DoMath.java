bf61e594a952832a69dbfb5d2d1a267b1e64a5e2
FoyArthur <20FoyA06@stu.smuhsd.org>
2018-09-14 09:01:00 -0700

    commit

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -7,5 +7,10 @@ public class DoMath {
 	//Returns the square of an integer
 	public static void main(String[] args) {
 		System.out.println(Calculate.square(5));
+		System.out.println(Calculate.cube(3));
+		System.out.println(Calculate.average(3.0 , 7.0));
+		System.out.println(Calculate.average(3.0, 5.0, 7.0));
+		System.out.println(toRadians())
 	}
+
 }
