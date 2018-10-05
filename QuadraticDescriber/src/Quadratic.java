42eafe71d7e846bc16895c26f117738f18472577
FoyArthur <20FoyA06@stu.smuhsd.org>
2018-10-05 09:50:00 -0700

    commit

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -6,19 +6,24 @@ public class Quadratic {
 	
 	public static String quadrDescriber (double a, double b, double c) {
 		String directionOpens;
-		if(x > 0) {
+		if(a > 0) {
 			directionOpens = "up";
 		} else {
 			directionOpens = "down";
 		}
-		String vertex;
-		
+		String intercepts;
+		intercepts = quadForm(a,b,c);
+		double x = round2(-b / (2 * a));
+		double y = round2(a * x * x + b * x + c);
+		String vertex = x + "and " + y;
+		return ("Opens:" + directionOpens + "\nVertex: " + x + ", " +  y + "\nIntercepts: " + intercepts);
+				
 	}
 	
 	//returns the real roots, if there any, of a quadratic equation. If there are no real roots says that there are no real roots.
 			//throws exception if num1 is zero
-			public static String quadForm (int num1, int num2, int num3) {
-				double numDiscriminant = Calculate.discriminant(num1, num2, num3);
+			public static String quadForm (double num1, double num2, double num3) {
+				double numDiscriminant = discriminant(num1, num2, num3);
 				
 				//no roots
 				if(numDiscriminant < 0) {
@@ -33,13 +38,63 @@ public class Quadratic {
 					return ((num2 * -1.0)/(2 * num1) + "");
 				} else {
 					//two roots
-					double firstRealRoot = ((-num2 + Calculate.sqrt(numDiscriminant))/ (2 * num1));
-					double secondRealRoot = ((-num2 - Calculate.sqrt(numDiscriminant))/(2 * num1));
-					if(Calculate.max(firstRealRoot, secondRealRoot) == firstRealRoot) {
-						return (Calculate.round2(secondRealRoot) + " and " + Calculate.round2(firstRealRoot));
+					double firstRealRoot = ((-num2 + sqrt(numDiscriminant))/ (2 * num1));
+					double secondRealRoot = ((-num2 - sqrt(numDiscriminant))/(2 * num1));
+					if(max(firstRealRoot, secondRealRoot) == firstRealRoot) {
+						return (round2(secondRealRoot) + " and " + round2(firstRealRoot));
 					} else {
-						return (Calculate.round2(firstRealRoot) + " and " + Calculate.round2(secondRealRoot));
+						return (round2(firstRealRoot) + " and " + round2(secondRealRoot));
 					}
+				}
+			}
+	
+			public static double max(double num1, double num2) {
+				if(num1 >= num2) {
+					return num1;
+				} else {
+					return num2;
+				}
+			}
+			
+			//returns value of discriminant based off the coeeficients of a quadratic equation
+			public static double discriminant(double numberA, double numberB, double numberC) {
+				return (numberB * numberB) - (4 * numberA * numberC);
+			}
+			
+			//returns an approximation of the square root of a double. Throws exception if radicand is less than zero.
+			public static double sqrt(double radicand){
+				if(radicand < 0) throw new IllegalArgumentException ("The radicand cannot be negative.");
+					double squareRoot = radicand;
+				while(absValue(radicand - (squareRoot * squareRoot)) >= 0.005){
+					squareRoot = (0.5 * (radicand/squareRoot + squareRoot));
+				}
+				return round2(squareRoot);
+			}
+			
+			//rounds a double correctly to 2 decimal places
+			public static double round2(double number){
+				double rounded = number * 100;
+				if(rounded % 1 >= 0.5){
+					int roundedNum = (int) rounded + 1;
+					double finalNum = (double) roundedNum/100.0;
+					return finalNum;
+				} else if(rounded % 1 <= -0.5){
+					int roundedNum = (int) rounded - 1;
+					double finalNum = (double) roundedNum/100.0;
+					return finalNum;
+				} else {
+					int roundedNum = (int) rounded;
+					double finalNum = (double) roundedNum/100.0;
+					return finalNum;
+					
+				}
+			}
+			
+			public static double absValue(double number) {
+				if(number < 0) {
+					return number * -1;
+				}else {
+					return number;
 				}
 			}
 
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -7,15 +7,24 @@ import java.util.Scanner;
 public class QuadraticClient {
 	
 	public static void main(String[] args) {
-		Scanner userInput = new Scanner (System.in);
-			System.out.println("What is a");
-			int a = userInput.nextInt();	
-			System.out.println("What is b");
-			int b = userInput.nextInt();
-			System.out.println("What is c");
-			int c = userInput.nextInt();
-			System.out.println(Quadratic.quadrDescriber(a,b,c));
+		String keepGoing = true;
 		
+			Scanner userInput = new Scanner (System.in);
+			while(keepGoing == true) {
+			System.out.print("a:");
+			int a = userInput.nextInt();	
+			System.out.print("b:");
+			int b = userInput.nextInt();
+			System.out.print("c:");
+			int c = userInput.nextInt();
+			System.out.print(Quadratic.quadrDescriber(a,b,c));
+			System.out.print("\nDo you want to keep going? Type \"quit\" to end");
+			String keepGoing = userInput.nextLine();
+			if(keepGoing == "quit") {
+				keepGoing = false;
+			}
+		
+		}
 	}
 
 }
