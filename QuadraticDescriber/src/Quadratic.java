027834f96dbfd1dfa972813787f173d056a59994
FoyArthur <20FoyA06@stu.smuhsd.org>
2018-10-26 01:59:20 -0400

    c

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -97,5 +97,40 @@ public class Quadratic {
 					return number;
 				}
 			}
+			
+			public static void backSlash{
+				System.out.print("\\");
+				
+			}
+			
+			public static void forwardSlash{
+				System.out.print("/");
+			}
+			public static void topHalf{
+				for(int i = 8; i >= 2; i-=2){
+					for(int j = 8; j > i; j--){
+						System.out.print(" ");
+					}
+					System.out.print("\\"");
+					for(int x = 1 ; x != i; x++){
+						System.out.print(":");
+					}
+					System.out.println("/");
+				
+				}
+			}
+			
+			public static void bottomHalf{
+				for(int i = 2; i <= 8; i+=2){
+					for(int j = 2; i > j; j++){
+						System.out.print(" ");
+					}
+					System.out.print("\"");
+					for(int x = 1 ; x != i; x++){
+						System.out.print(":");
+					}
+					System.out.println("\\");
+				
+			}
 
 }
